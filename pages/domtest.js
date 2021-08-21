ac1c0c84a6592b590eda66da5fea9d22d50d0bac
santiago hernandez <santiagohernandezc@gmail.com>
2021-08-21 13:31:42 -0500

    Nuevos atributos y ejercicio de DOM
    Nuevos atributos y ejercicio de DOM

---
+++ pages/domtest.js
@@ -0,0 +1,42 @@
+/*
+let btn = document.querySelector("btn")
+
+const ingresar = () => {
+
+    let user = document.querySelector("#user").value
+    let pass = document.querySelector("#pass").value
+    
+    console.log('Ingresaste Correctamente' ${user} ${pass});
+
+}
+
+btn.addEventListener("click", () => {
+
+    ingresar()
+
+})
+
+*/
+/*
+let nombre = document.getElementById("user");
+let clave = document.getElementById("password");
+let btn = document.getElementById("btn");
+let form1 = document.getElementById("form1");
+
+const ingresar = (e) => {
+
+    e.preventDefault();
+
+    let user = document.getElementById("user");
+    let pass = document.getElementById("password");
+
+    console.log(`Ingresaste correctamente ${user.value} ${pass.value}`);
+
+}
+
+form1.addEventListener("submit", ingresar);
+
+*/
+
+let clase = document.getElementsByClassName("row")
+console.log(clase)
